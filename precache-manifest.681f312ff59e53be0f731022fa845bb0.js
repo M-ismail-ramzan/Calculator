bea6730574dad548427f8f561306eef0d3f89274
ISMAIL RAMZAN <m-ismail-ramzan@yahoo.com>
2019-08-08 00:01:53 +0500

    Updates

---
+++ precache-manifest.681f312ff59e53be0f731022fa845bb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e01373cf5813cd9bfe173ba3f3b230a",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "1a1648f53e2cbd533091",
+    "url": "/Calculator/static/css/main.edfcb32f.chunk.css"
+  },
+  {
+    "revision": "5d6c8231a43e5a1c7fe8",
+    "url": "/Calculator/static/js/2.9661121e.chunk.js"
+  },
+  {
+    "revision": "1a1648f53e2cbd533091",
+    "url": "/Calculator/static/js/main.0c90116a.chunk.js"
+  },
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  }
+]);
